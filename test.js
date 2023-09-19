d999d88b322e8398ae15858bd33b3e0706492a01
Givi23 <givi023@mail.ru>
2023-09-19 09:33:37 +0300

    second  commit

---
+++ test.js
@@ -0,0 +1,55 @@
+//д/з 14.09.23
+// const upBtn = document.querySelector('.up-button')
+// const downBtn = document.querySelector('.down-button')
+// const sidebar = document.querySelector('.sidebar')
+// const mainSlide = document.querySelector('.main-slide')
+// const container = document.querySelector('.container')
+
+// const slidesCount = mainSlide.querySelectorAll('div').length
+// console.log(slidesCount)
+// // арефмитические выражения встовлять или переменную`${}`
+// sidebar.style.top = `${(slidesCount - 1) * 100}vh`;
+
+// upBtn.addEventListener('click', ()=>{
+// console.log(`${3+1} up`)
+// })
+
+// downBtn.addEventListener('click', ()=>{
+//     console.log(`${5+3} down`)
+//     })
+
+// sidebar.style.top = `-300vh` 
+
+//д/з 19.09.23
+// №1 
+//операция (+)
+// const a = 25;
+// const b = 83;
+// console.log(`${a + b}`);
+
+// операция (*)
+// const a = 7;
+// const b = 88;
+// console.log(`${a * b}`);
+
+// операция (/)
+// const a = 88;
+// const b = 4;
+// console.log(`${a / b}`);
+
+// операция (-)
+// const a = 88;
+// const b = 47;
+// console.log(`${a - b}`);
+
+// №2 
+// const middleBtn = document.querySelector('.middle-button');
+// console.log(middleBtn)
+// middleBtn.addEventListener('click',middle-button);
+// №3
+const downButton = document.querySelector('.down-button');
+console.log(downButton)
+// downButton.addEventListener('click', ()=>{ 
+// alert("Нажми чтобы спрятать")
+// })
+
