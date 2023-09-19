b82dbea205b26ac518d96c0cb9ad0e04b0288549
Givi23 <givi023@mail.ru>
2023-09-19 09:55:38 +0300

    19.09.23

--- test.js
+++ test.js
@@ -1,55 +1,29 @@
-//д/з 14.09.23
-// const upBtn = document.querySelector('.up-button')
-// const downBtn = document.querySelector('.down-button')
-// const sidebar = document.querySelector('.sidebar')
-// const mainSlide = document.querySelector('.main-slide')
-// const container = document.querySelector('.container')
 
-// const slidesCount = mainSlide.querySelectorAll('div').length
-// console.log(slidesCount)
-// // арефмитические выражения встовлять или переменную`${}`
-// sidebar.style.top = `${(slidesCount - 1) * 100}vh`;
+const upBtn = document.querySelector('.up-button')
+const downBtn = document.querySelector('.down-button')
+const sidebar = document.querySelector('.sidebar')
+const mainSlide = document.querySelector('.main-slide')
+const container = document.querySelector('.container')
+const slidesCount = mainSlide.querySelectorAll('div').length
 
-// upBtn.addEventListener('click', ()=>{
-// console.log(`${3+1} up`)
-// })
+let activeIndexSlide = 0
 
-// downBtn.addEventListener('click', ()=>{
-//     console.log(`${5+3} down`)
-//     })
 
-// sidebar.style.top = `-300vh` 
 
-//д/з 19.09.23
-// №1 
-//операция (+)
-// const a = 25;
-// const b = 83;
-// console.log(`${a + b}`);
+// арефмитические выражения встовлять или переменную`${}`
+sidebar.style.top = `${(slidesCount - 1) * 100}vh`;
 
-// операция (*)
-// const a = 7;
-// const b = 88;
-// console.log(`${a * b}`);
+upBtn.addEventListener('click', ()=>{
+    changeSlide('up')
+})
 
-// операция (/)
-// const a = 88;
-// const b = 4;
-// console.log(`${a / b}`);
 
-// операция (-)
-// const a = 88;
-// const b = 47;
-// console.log(`${a - b}`);
+downBtn.addEventListener('click', ()=>{
+    changeSlide('down')
+    })
 
-// №2 
-// const middleBtn = document.querySelector('.middle-button');
-// console.log(middleBtn)
-// middleBtn.addEventListener('click',middle-button);
-// №3
-const downButton = document.querySelector('.down-button');
-console.log(downButton)
-// downButton.addEventListener('click', ()=>{ 
-// alert("Нажми чтобы спрятать")
-// })
 
+
+function changeSlide(direction){
+    
+}
